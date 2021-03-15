7932780141cb3f79fbb271afd22abd785db73a6b
Ignacio Inda <ignacioinda@Macbook-Inda-245.local>
2021-03-15 03:39:41 -0300

    test webhook3

--- app.js
+++ app.js
@@ -37,11 +37,11 @@ app.get('/success', (req, res)=> res.render('success', req.query));
 
 app.post('/notifications', (request, response) => {
     console.log('----------- webhook -----------');
-    console.log(request.query);
     console.log(request.body);
     console.log('----------- webhook -----------');
-    response.sendStatus(200);
     response.send(request.body);
+    response.status(200).end() // Responding is important
+
 });
 
 app.post('/checkout', (request, response) =>{
