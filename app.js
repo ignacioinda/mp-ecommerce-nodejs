4338eb119e8782a8e4fdf9e2a3b8fd50e1eaca92
Ignacio Inda <ignacioinda@Macbook-Inda-245.local>
2021-03-15 00:51:44 -0300

    test heroku

--- app.js
+++ app.js
@@ -1,8 +1,25 @@
 var express = require('express');
 var exphbs  = require('express-handlebars');
-var port = process.env.PORT || 3000
+const dotenv = require('dotenv');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const mercadopago = require('mercadopago');
+
+dotenv.config();
+
+mercadopago.configure({
+    access_token: "APP_USR-6317427424180639-042414-47e969706991d3a442922b0702a0da44-469485398",
+    integrator_id: "dev_24c65fb163bf11ea96500242ac130004"
+});
+
+const port = process.env.PORT || 3000
 
 var app = express();
+
+app.use(cors());
+
+app.use(bodyParser.json());
  
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
@@ -11,12 +28,35 @@ app.use(express.static('assets'));
  
 app.use('/assets', express.static(__dirname + '/assets'));
 
-app.get('/', function (req, res) {
-    res.render('home');
+app.get('/', (req, res)=> res.render('home'));
+
+app.get('/detail', (req, res)=> res.render('detail', req.query));
+app.get('/pending', (req, res)=> res.render('pending', req.query));
+app.get('/failure', (req, res)=> res.render('failure', req.query));
+
+app.post('/notifications', (request, response) => {
+    //const data = request.body;
+    /*mercadopago.ipn.manage(request.body)
+    .then(data => response.send(data))
+    .then((error) => console.log(error));*/
+    console.log(request.body);
+    response.send(request.body);
+});
+/*Ejemplo: Si configuraste la notification_url: 
+https://www.yoursite.com/notifications, 
+recibirás notificaciones de pago de esta manera: 
+https://www.yoursite.com/notifications?topic=payment&id=123456789 */
+
+app.post('/checkout', (request, response) =>{
+    const preference = request.body;
+
+    mercadopago.preferences.create(preference)
+    .then(res => {
+        global.id = res.body.id;
+        console.log(res.body);
+        response.send(res.body);
+    });
 });
 
-app.get('/detail', function (req, res) {
-    res.render('detail', req.query);
-});
 
 app.listen(port);
---
+++ assets/details.js
@@ -0,0 +1,68 @@
+const btnPayment = document.getElementById('checkout');
+
+let preference = {
+    items: [
+      {
+        id: 1234,
+        title: document.getElementById('title').innerHTML,
+        description: 'Dispositivo móvil de Tienda e-commerce',
+        picture_url: document.getElementById('image').src,
+        quantity: parseInt(document.getElementById('unit').innerHTML),
+        currency_id : 'ARS',
+        unit_price: parseFloat(document.getElementById('price').innerHTML),
+      }
+    ],
+    payer: {
+        name: 'Lalo',
+        surname: 'Landa',
+        email: 'test_user_63274575@testuser.com',
+        phone: {
+            area_code: '11',
+            number: 22223333
+        },
+        address: {
+            street_name: "False",
+            street_number: 123,
+            zip_code: '1111'
+        }
+    },
+    back_urls: {
+        success: 'https://mpmercadolibre.herokuapp.com/',
+        failure: 'https://mpmercadolibre.herokuapp.com/failure',
+        pending: 'https://mpmercadolibre.herokuapp.com/pending'
+    },
+    auto_return: 'approved',
+    payment_methods: {
+        excluded_payment_methods: [
+            {
+                id: 'amex'
+            }
+        ],
+        excluded_payment_types: [
+            {
+                id: 'atm'
+            }
+        ],
+        installments: 6
+    },
+    notification_url: 'https://mpmercadolibre.herokuapp.com/notifications',
+    external_reference: 'ignacioinda@gmail.com'
+}
+
+const payment = ()=>{
+    //console.log(preference);
+    fetch('https://mpmercadolibre.herokuapp.com/checkout', {
+        method: 'post',
+        body: JSON.stringify(preference),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    }).then(res => res.json()).then(payment => {
+        console.log(payment);
+        //window.open(payment.init_point);
+        window.open(payment.init_point,'_self');
+    });
+}
+
+
+btnPayment.onclick = payment;
